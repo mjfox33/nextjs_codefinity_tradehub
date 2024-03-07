fc1cafe4d92b25ac79f61cea13560ced38ceb3b0
Michael Fox <mjfox33@gmail.com>
2024-03-06 23:44:01 -0500

    updated invoices to show top 6

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,14 +1,12 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import IncomeChart from '@/app/ui/dashboard/income-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { poppins } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { IncomeChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { CardsSkeleton, IncomeChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-    const { numberOfSellers, numberOfInvoices, totalFulfilledInvoices, totalAwaitingInvoices } = await fetchCardData();
-
     return (
         <main className='rounded-xl bg-neutral-900 p-6'>
             <h1 className={`${poppins.className} mb-4 text-center text-xl text-white md:text-3xl`}
@@ -16,14 +14,9 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {<Card title="Earned" value={totalFulfilledInvoices} type="earned" />}
-                {<Card
-                    title="In Progress"
-                    value={totalAwaitingInvoices}
-                    type="awaiting"
-                />}
-                {<Card title="All Invoices" value={numberOfInvoices} type="invoices" />}
-                {<Card title="Total Sellers" value={numberOfSellers} type="sellers" />}
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<IncomeChartSkeleton />}>
